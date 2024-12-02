1845475015ca70fe2b7df1b9d10575ac76256770
Supreme Raut <supremeraut94@gmail.com>
2024-12-02 08:04:39 +0545

    created node class

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 class Node {
   constructor(node) {
     this.node = node;
+    this.left = null;
+    this.right = null;
+  }
+  setLeft(left) {
+    this.left = left;
+  }
+  setRight(right) {
+    this.right = right;
+  }
+}
+class Tree {
+  constructor(array) {
+    this.array = [...new Set(array)].sort((a, b) => a - b);
+    this.root = null;
   }
 }

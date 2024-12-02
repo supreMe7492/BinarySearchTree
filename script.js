b7bb2cc9d72d3cba9c820d05a506fd1fcae83327
Supreme Raut <supremeraut94@gmail.com>
2024-12-02 09:30:30 +0545

    added method to build Tree

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 class Node {
-  constructor(node) {
-    this.node = node;
+  constructor(data) {
+    this.data = data;
     this.left = null;
     this.right = null;
-  }
-  setLeft(left) {
-    this.left = left;
-  }
-  setRight(right) {
-    this.right = right;
   }
 }
 class Tree {
   constructor(array) {
     this.array = [...new Set(array)].sort((a, b) => a - b);
-    this.root = null;
+    this.root = this.buildTree(this.array);
+    this.prettyPrint(this.root);
+  }
+  buildTree(array) {
+    const start = 0;
+    const end = array.length - 1;
+    const mid = Math.floor((start + end) / 2);
+
+    if (start > end) {
+      return null;
+    } else {
+      const root = new Node(array[mid]);
+      root.left = this.buildTree(array.slice(start, mid));
+      root.right = this.buildTree(array.slice(mid + 1));
+      return root;
+    }
+  }
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }
+
+let b = [1, 2, 3, 4, 5, 6, 7];
+let a = new Tree(b);
